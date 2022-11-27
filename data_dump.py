9f014cb72f94c654cd12131926a7ba58bf37911d
Shobhandeb <devshobhan4@gmail.com>
2022-11-27 21:36:55 +0530

    Data Ingestion Module Done

--- data_dump.py
+++ data_dump.py
@@ -1,23 +1,31 @@
 import pymongo
 import pandas as pd
 import json
-
 # Provide the mongodb localhost url to connect python to mongodb.
 client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
 
-DATA_FILE_PATH = "aps_failure_training_set1.csv"
-DATABASE_NAME = "aps"
-COLLECTION_NAME = "sensor"
+DATA_FILE_PATH="/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME="aps"
+COLLECTION_NAME="sensor"
+
 
 if __name__=="__main__":
     df = pd.read_csv(DATA_FILE_PATH)
-    print(f"Rows and Columns: {df.shape}")
+    print(f"Rows and columns: {df.shape}")
 
-# Convert the Dataframe into json to dump these records into MongoDB
+    #Convert dataframe to json so that we can dump these record in mongo db
     df.reset_index(drop=True,inplace=True)
-    # json_record = df.T.to_json()
+
     json_record = list(json.loads(df.T.to_json()).values())
     print(json_record[0])
 
-# Insert converted json record to MongoDB
+    #insert converted json record to mongo db
     client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+
+
+
+
+
+
+
